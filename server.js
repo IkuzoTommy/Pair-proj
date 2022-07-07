79baa2aa14284af2ce58008ea01f6e95fbef6b44
IkuzoTommy <101907933+IkuzoTommy@users.noreply.github.com>
2022-07-07 17:32:42 -0400

    add clientside files

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+//REQUIRE DEPENDENCIES
 const express = require ('express');
 const app = express();
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 
+//SET UP DATABASE VARIABES
 let db,
     dbConnectionString = process.env.DB_STRING,
     dbName = 'pair-code',
@@ -15,6 +17,11 @@ MongoClient.connect(dbConnectionString)
         db = client.db(dbName)
         collection = db.collection('pair-code')
     })
+
+//SETS UP THE PORT, VARIABLES FOR SETTING MIDDLEWARE
+app.set('view engine', 'ejs') //sets up the view engine to render ejs -> html
+app.use(express.json()) // lets express parse the json data
+app.use(express.static('public')) //client side files
 
 app.listen(process.env.PORT || PORT, () => {
     console.log('That THANG is thangin.')
