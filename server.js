f32be5618cdc099b7ebbd5b80dd20d83b30ab63a
IkuzoTommy <101907933+IkuzoTommy@users.noreply.github.com>
2022-07-07 17:26:46 -0400

    Server Live

--- server.js
+++ server.js
@@ -4,4 +4,21 @@ const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 
-let 
+let db,
+    dbConnectionString = process.env.DB_STRING,
+    dbName = 'pair-code',
+    collection;
+
+MongoClient.connect(dbConnectionString)
+    .then( client => {
+        console.log(`connnected to ${dbName}`)
+        db = client.db(dbName)
+        collection = db.collection('pair-code')
+    })
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log('That THANG is thangin.')
+})
+
+
+
